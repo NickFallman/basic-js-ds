f86dcb93444cf617cd9aaf12361de0a26ef262cc
NickFallman <nickf@tut.by>
2022-10-18 02:59:06 +0300

    feat: queue test done

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -13,21 +13,66 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.dequeue(); // returns the top element from queue and deletes it, returns 1
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
-class Queue {
+// class Queue {
 
+//   getUnderlyingList() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+
+//   enqueue(/* value */) {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+
+//   dequeue() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+// }
+class Queue {
+  constructor() {
+    this.queueHead = null;
+    this.queueLength = 0;
+  }
+  // class ListNode {
+  //   constructor(x) {
+  //     this.value = x;
+  //     this.next = null;
+  //   }
+  // }
   getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    // return JSON.stringify(this.queueHead);
+    return this.queueHead;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  enqueue( value ) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let node = new ListNode(value);
+    if (this.queueLength === 0) {
+      this.queueHead = node;
+    } else {
+      let current = this.queueHead;
+      while(current.next) {
+        current = current.next;
+      }
+      current.next = new ListNode(value);
+    }
+    this.queueLength++;
+    return this.queueHead;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let current = this.queueHead; // now current is the head of the Linked List
+    this.queueHead = current.next;
+    // console.log(this.queueHead);
+    this.queueLength--;
+    return current.value;
   }
 }
 
